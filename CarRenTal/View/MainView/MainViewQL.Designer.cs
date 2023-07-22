b64109103dcecb46240f7cbe4b222a2a82b5ac6b
Phúc Nguyễn Khắc <phucnkph30253@fpt.edu.vn>
2023-07-22 21:26:53 +0700

    a

---
+++ CarRenTal/View/MainView/MainViewQL.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace CarRenTal.View.MainView
+{
+    partial class MainViewQL
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            grb_Funtion = new GroupBox();
+            btnAccount = new Button();
+            btnQLTC = new Button();
+            btnQLKH = new Button();
+            btnQLNV = new Button();
+            btnQLHD = new Button();
+            btnQLX = new Button();
+            btnQLCTX = new Button();
+            pn_main = new Panel();
+            btnHome = new Button();
+            grb_Funtion.SuspendLayout();
+            SuspendLayout();
+            // 
+            // grb_Funtion
+            // 
+            grb_Funtion.Controls.Add(btnAccount);
+            grb_Funtion.Controls.Add(btnQLTC);
+            grb_Funtion.Controls.Add(btnQLKH);
+            grb_Funtion.Controls.Add(btnQLNV);
+            grb_Funtion.Controls.Add(btnQLHD);
+            grb_Funtion.Controls.Add(btnQLX);
+            grb_Funtion.Controls.Add(btnQLCTX);
+            grb_Funtion.Location = new Point(11, 293);
+            grb_Funtion.Name = "grb_Funtion";
+            grb_Funtion.Size = new Size(352, 639);
+            grb_Funtion.TabIndex = 0;
+            grb_Funtion.TabStop = false;
+            // 
+            // btnAccount
+            // 
+            btnAccount.FlatAppearance.BorderSize = 0;
+            btnAccount.FlatStyle = FlatStyle.Flat;
+            btnAccount.Font = new Font("Calibri", 16F, FontStyle.Bold, GraphicsUnit.Point);
+            btnAccount.Location = new Point(16, 492);
+            btnAccount.Name = "btnAccount";
+            btnAccount.Size = new Size(320, 62);
+            btnAccount.TabIndex = 7;
+            btnAccount.Text = "Tài khoản";
+            btnAccount.TextAlign = ContentAlignment.MiddleLeft;
+            btnAccount.UseVisualStyleBackColor = true;
+            btnAccount.Click += btnAccount_Click;
+            // 
+            // btnQLTC
+            // 
+            btnQLTC.FlatAppearance.BorderSize = 0;
+            btnQLTC.FlatStyle = FlatStyle.Flat;
+            btnQLTC.Font = new Font("Calibri", 16F, FontStyle.Bold, GraphicsUnit.Point);
+            btnQLTC.Location = new Point(16, 415);
+            btnQLTC.Name = "btnQLTC";
+            btnQLTC.Size = new Size(320, 62);
+            btnQLTC.TabIndex = 6;
+            btnQLTC.Text = "Quản lý thu chi";
+            btnQLTC.TextAlign = ContentAlignment.MiddleLeft;
+            btnQLTC.UseVisualStyleBackColor = true;
+            btnQLTC.Click += btnQLTC_Click;
+            // 
+            // btnQLKH
+            // 
+            btnQLKH.FlatAppearance.BorderSize = 0;
+            btnQLKH.FlatStyle = FlatStyle.Flat;
+            btnQLKH.Font = new Font("Calibri", 16F, FontStyle.Bold, GraphicsUnit.Point);
+            btnQLKH.Location = new Point(16, 338);
+            btnQLKH.Name = "btnQLKH";
+            btnQLKH.Size = new Size(320, 62);
+            btnQLKH.TabIndex = 5;
+            btnQLKH.Text = "Quản lý khách hàng";
+            btnQLKH.TextAlign = ContentAlignment.MiddleLeft;
+            btnQLKH.UseVisualStyleBackColor = true;
+            btnQLKH.Click += btnQLKH_Click;
+            // 
+            // btnQLNV
+            // 
+            btnQLNV.FlatAppearance.BorderSize = 0;
+            btnQLNV.FlatStyle = FlatStyle.Flat;
+            btnQLNV.Font = new Font("Calibri", 16F, FontStyle.Bold, GraphicsUnit.Point);
+            btnQLNV.Location = new Point(16, 261);
+            btnQLNV.Name = "btnQLNV";
+            btnQLNV.Size = new Size(320, 62);
+            btnQLNV.TabIndex = 4;
+            btnQLNV.Text = "Quản lý nhân viên";
+            btnQLNV.TextAlign = ContentAlignment.MiddleLeft;
+            btnQLNV.UseVisualStyleBackColor = true;
+            btnQLNV.Click += btnQLNV_Click;
+            // 
+            // btnQLHD
+            // 
+            btnQLHD.FlatAppearance.BorderSize = 0;
+            btnQLHD.FlatStyle = FlatStyle.Flat;
+            btnQLHD.Font = new Font("Calibri", 16F, FontStyle.Bold, GraphicsUnit.Point);
+            btnQLHD.Location = new Point(16, 184);
+            btnQLHD.Name = "btnQLHD";
+            btnQLHD.Size = new Size(320, 62);
+            btnQLHD.TabIndex = 3;
+            btnQLHD.Text = "Quản lý hóa đơn";
+            btnQLHD.TextAlign = ContentAlignment.MiddleLeft;
+            btnQLHD.UseVisualStyleBackColor = true;
+            btnQLHD.Click += btnQLHD_Click;
+            // 
+            // btnQLX
+            // 
+            btnQLX.FlatAppearance.BorderSize = 0;
+            btnQLX.FlatStyle = FlatStyle.Flat;
+            btnQLX.Font = new Font("Calibri", 16F, FontStyle.Bold, GraphicsUnit.Point);
+            btnQLX.Location = new Point(16, 107);
+            btnQLX.Name = "btnQLX";
+            btnQLX.Size = new Size(320, 62);
+            btnQLX.TabIndex = 2;
+            btnQLX.Text = "Quản lý xe";
+            btnQLX.TextAlign = ContentAlignment.MiddleLeft;
+            btnQLX.UseVisualStyleBackColor = true;
+            btnQLX.Click += btnQLX_Click;
+            // 
+            // btnQLCTX
+            // 
+            btnQLCTX.FlatAppearance.BorderSize = 0;
+            btnQLCTX.FlatStyle = FlatStyle.Flat;
+            btnQLCTX.Font = new Font("Calibri", 16F, FontStyle.Bold, GraphicsUnit.Point);
+            btnQLCTX.Location = new Point(16, 30);
+            btnQLCTX.Name = "btnQLCTX";
+            btnQLCTX.Size = new Size(320, 62);
+            btnQLCTX.TabIndex = 1;
+            btnQLCTX.Text = "Quản lý cho thuê xe";
+            btnQLCTX.TextAlign = ContentAlignment.MiddleLeft;
+            btnQLCTX.UseVisualStyleBackColor = true;
+            btnQLCTX.Click += button1_Click;
+            // 
+            // pn_main
+            // 
+            pn_main.Location = new Point(381, 98);
+            pn_main.Name = "pn_main";
+            pn_main.Size = new Size(1500, 840);
+            pn_main.TabIndex = 2;
+            pn_main.Paint += panel1_Paint;
+            // 
+            // btnHome
+            // 
+            btnHome.Location = new Point(37, 12);
+            btnHome.Name = "btnHome";
+            btnHome.Size = new Size(305, 193);
+            btnHome.TabIndex = 3;
+            btnHome.UseVisualStyleBackColor = true;
+            btnHome.Click += btnHome_Click;
+            // 
+            // MainViewQL
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.Main2;
+            ClientSize = new Size(1892, 944);
+            Controls.Add(btnHome);
+            Controls.Add(pn_main);
+            Controls.Add(grb_Funtion);
+            Name = "MainViewQL";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Car Rental";
+            Load += MainViewQL_Load;
+            grb_Funtion.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox grb_Funtion;
+        private Button btnQLCTX;
+        private Panel pn_main;
+        private Button btnAccount;
+        private Button btnQLTC;
+        private Button btnQLKH;
+        private Button btnQLNV;
+        private Button btnQLHD;
+        private Button btnQLX;
+        private Button btnHome;
+    }
+}
