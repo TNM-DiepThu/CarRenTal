53c7eba29e9cc9dcd98c6e506db0339aaaed35b0
FPTSHOP <FPTSHOP@HUNG>
2023-07-23 16:35:31 +0700

    upadate_login_hung

--- CarRenTal/Program.cs
+++ CarRenTal/Program.cs
@@ -1,3 +1,5 @@
+using CarRenTal.View.ViewLogin;
+
 namespace CarRenTal
 {
     internal static class Program
@@ -11,7 +13,7 @@ namespace CarRenTal
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new View.HangXeView());
+            Application.Run(new MainLoginForm());
         }
     }
 }
