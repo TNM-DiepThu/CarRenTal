c5bcfc183a8202f07e3afc8ddf4b49deb0548317
LAPTOP-04RP8E0L\NDCT <minhtnph26266@fpt.edu.vn>
2023-07-25 11:06:36 +0700

    SuaDBLan12

---
+++ Dal/Migrations/20230725040618_DB12.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dal.Migrations
+{
+    public partial class DB12 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "NgaySinh",
+                table: "Khach_hang",
+                type: "DateTime",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NgaySinh",
+                table: "Khach_hang");
+        }
+    }
+}
