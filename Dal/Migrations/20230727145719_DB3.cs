277562a379e49eca4a57a543380b804f25de0bbf
LAPTOP-04RP8E0L\NDCT <minhtnph26266@fpt.edu.vn>
2023-07-27 21:59:14 +0700

    sua DB

---
+++ Dal/Migrations/20230727145719_DB3.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dal.Migrations
+{
+    public partial class DB3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SoKhung",
+                table: "Xe");
+
+            migrationBuilder.DropColumn(
+                name: "SoMay",
+                table: "Xe");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "SoKhung",
+                table: "Xe",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "SoMay",
+                table: "Xe",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
