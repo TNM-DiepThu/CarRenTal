452740d8f99924abc2700d7fba446ff35e4c4225
LAPTOP-04RP8E0L\NDCT <minhtnph26266@fpt.edu.vn>
2023-07-27 07:47:57 +0700

    SUA DB

---
+++ Dal/Migrations/20230727004531_BB2.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dal.Migrations
+{
+    public partial class BB2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TrangThai",
+                table: "Đang_kiem");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "ChiPhi",
+                table: "Đang_kiem",
+                type: "DECIMAL",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ChiPhi",
+                table: "Đang_kiem");
+
+            migrationBuilder.AddColumn<int>(
+                name: "TrangThai",
+                table: "Đang_kiem",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
