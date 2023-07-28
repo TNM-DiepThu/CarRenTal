a5c5cc5becb64e34c03c688ea15a382c2f542333
FPTSHOP <FPTSHOP@HUNG>
2023-07-28 21:13:41 +0700

    edit Them xe

--- CarRenTal/View/QuanLiXe/ThemXe.cs
+++ CarRenTal/View/QuanLiXe/ThemXe.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Dal.Repository;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,39 +13,43 @@ namespace CarRenTal.View.QuanLiXe
 {
     public partial class ThemXe : Form
     {
+        LoaiXeRepo loaiXeRepo = new LoaiXeRepo();
+        MauSacRepo mauSacRepo = new MauSacRepo();
+        XeRepo XeRepo = new XeRepo();
         public ThemXe()
         {
             InitializeComponent();
+            cbb_loaiXe.DataSource = loaiXeRepo.GetALL();
+            cbb_loaiXe.DisplayMember = "Name";
+            cbb_loaiXe.ValueMember = "Id";
+            cbb_mau.DataSource = mauSacRepo.GetALL();
+            cbb_mau.DisplayMember = "TenMauSac";
+            cbb_mau.ValueMember = "Id";
         }
 
-        private void label2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-
-        }
 
         private void ThemXe_Load(object sender, EventArgs e)
         {
+            dataGridView1.DataSource = XeRepo.GetXe();
 
         }
 
-        private void textBox7_TextChanged(object sender, EventArgs e)
+        private void bt_add_Click(object sender, EventArgs e)
         {
-
-        }
-
-        private void label9_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label13_Click(object sender, EventArgs e)
-        {
-
+            XeRepo.CreateXe(new Dal.Modal.Xe()
+            {
+                BienSo=tx_bienSo.Text,
+                SoCongTo= int.Parse(tx_congTo.Text),
+                SoKhung= tx_soKhung.Text,
+                SoMay=tx_soMay.Text,
+                ID= Guid.NewGuid(),
+                DonGia= decimal.Parse(tx_donGia.Text),
+                IdLoaiXe= Guid.Parse(cbb_loaiXe.SelectedValue.ToString()),
+                IdMauSac= Guid.Parse(cbb_mau.SelectedValue.ToString()),
+                TrangThai= 1,
+            });
+            ThemXe_Load(null, null);
         }
     }
+
 }
