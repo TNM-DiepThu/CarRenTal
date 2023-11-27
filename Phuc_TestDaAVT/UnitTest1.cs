415774a8bf3a8405524da8cdff334ab2c26c7c2a
Hoangzero8688 <140143143+Hoangzero8688@users.noreply.github.com>
2023-11-27 13:48:59 +0700

    Change Demo My test

---
+++ Phuc_TestDaAVT/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Phuc_TestDaAVT
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
